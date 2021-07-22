a55099fcf86742512e0c61791813f8003f79aa5e
Abdulaziz <baxtiyorov1999@bk.ru>
2021-07-22 07:47:10 +0500

    update

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const fruits = [
+let fruits = [
     {id: 1, title: 'Яблоки', price: 20, img: './img/1345.jpg'},
     {id: 2, title: 'Апельсин', price: 30, img: './img/3cbf3bd41c_1000.jpg'},
     {id: 3, title: 'Манго', price: 40, img: './img/Mango-1.jpg'}
 ]
 
 const toHtml = fruit => `
-    <div class="col-md-4">
+    <div class="col-12 col-md-4">
         <div class="card">
         <img src=${fruit.img} class="card-img-top" style="height: 300px"  alt=${fruit.title}>
             <div class="card-body">
              <h5 class="card-title">${fruit.title}</h5>
-             <a href="" class="btn btn-primary">посмотреть цену</a>
-            <a href="" class="btn btn-danger">удалить</a>
+             <a href="" class="btn btn-primary" data-btn="price" data-id=${fruit.id}>посмотреть цену</a>
+            <a href="" class="btn btn-danger" data-btn="remove" data-id=${fruit.id}>удалить</a>
          </div>
     </div>
 </div>
@@ -32,22 +32,41 @@ render()
 
 
 
-const modal = $.modal({
-    title: 'My modal',
+const priceModal = $.modal({
+    title: 'Цена на товар',
     closable: false,
-    content: `
-        <h4>Modal is working</h4>
-        <p>modal is true</p>
-    `,
     width: '600px',
     footerButtons: [
-        {text: 'Ok', type: 'btn-primary', handler() {
-            modal.close()
+        {text: 'Закрыть', type: 'btn-danger', handler() {
+            priceModal.close()
                 console.log('primary btn clicked')
             }},
-        {text: 'Cancel', type: 'btn-danger', handler() {
-                modal.close()
-                console.log('Danger btn clicked')
-            }}
+
     ]
+})
+
+
+document.addEventListener('click', (event) => {
+    const btnType = event.target.dataset.btn
+    const id = +event.target.dataset.id
+    const fruit = fruits.find(f => f.id === id)
+    event.preventDefault()
+    if (btnType === 'price') {
+        priceModal.setContent(`
+            <p> цена на ${fruit.title} <strong>${fruit.price}$</strong></p>
+        `)
+        priceModal.open()
+        console.log(fruit, id)
+    }else if (btnType === 'remove') {
+        console.log('clicked')
+        $.confirm({
+            title: 'Вы уверены?',
+            content: `<p>Вы удаляете фрукт: <strong>${fruit.title}</strong></p>`
+        }).then(() => {
+            fruits = fruits.filter(f => f.id !== id)
+            render()
+        }).catch( () => {
+            console.log('Cancel')
+        })
+    }
 })
